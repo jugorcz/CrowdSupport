b908398b49828d3dc4b920b57e1d608216536576
Justyna Gorczyca <just.gorcz@gmail.com>
2019-02-15 14:10:16 +0100

    statistics first try

--- dataBaseConnection.py
+++ dataBaseConnection.py
@@ -12,18 +12,20 @@ def getConnection():
 
 
 #--------------------------------------------------------
-def findUser(userName):
+
+def getUserID():
+    print("Your login in Crowd app:")
+    userName = raw_input()
+
     cursor.execute("SELECT profilID FROM Profile WHERE name = '" + userName + "'")
     result = cursor.fetchone()
     if result is None:
         print("You are not user of Crowd app, bye!")
         sys.exit()
-    else:
-        print("user id: " + str(result[0]))
     return result[0]
 
 #--------------------------------------------------------
-def findAccessKey(userID, accessKey):
+def checkIsEmptyGame(userID, accessKey):
     cursor.execute("SELECT accessKey FROM Game where ownerID = " + str(userID))
 
     key = cursor.fetchone()
@@ -34,34 +36,62 @@ def findAccessKey(userID, accessKey):
             result = cursor.fetchone()
             if result[1] is None:
                 print("You can add new game.")
-                return True, True
+                return True, result[0]
             else:
-                print("This accessKey was already used for '" + result[1] + "' game.")
-                return True, False
+                print("This is accessKey for '" + result[1] + "' game.")
+                return False, result[0]
         key = cursor.fetchone()
-    return False, False
+
+    print("This accessKey doesn't exist, please enter valid accessKey.")
+    sys.exit()
+
 
 #--------------------------------------------------------
-
-def getUserData():
-    print("User login:")
-    userName = raw_input()
-    print("Hello " + userName)
-    userID = findUser(userName)
-
+def checkAccessKey(userID):
     print("Enter your access key: ")
     key = raw_input()
     accessKey = int(key)
     if accessKey == 0:
         print("Wrong key")
         sys.exit()
+    return checkIsEmptyGame(userID, accessKey)
 
-    print("Your key: " + str(accessKey))
-    keyEgsists, gameisEmpty = findAccessKey(userID, accessKey)
+#--------------------------------------------------------
+def findPossibleAnswers(questionID, cursor):
+    cursor.execute("SELECT * FROM Answer WHERE questionID = " + str(questionID))
+    answers = cursor.fetchall()
+    for answer in answers:
+        showed = answer[9]
+        chosen = answer[8]
+        percentage = 0
+        if chosen != 0 and showed != 0:
+            percentage = 100 * chosen / showed
+        print("\t" + str(answer[2]) + "\tshowed: " + str(answer[9]) + "\tchosen: " + str(answer[8]) + "\t" + str(percentage) + "%")
+
+
+
+#--------------------------------------------------------
+def generateStatistics(userID, gameID, cursor):
+    cursor.execute("SELECT * FROM Question WHERE gameID = " + str(gameID))
+    questions = cursor.fetchall()
+    questionNumber = 1
+    for question in questions:
+        print("\n" + str(questionNumber) + ": " + question[2])
+        findPossibleAnswers(question[0], cursor)
+        questionNumber += 1
+
+
 
 
 #--------------------------------------------------------
 import pyodbc
 import sys
 cursor = getConnection()
-getUserData()
+userID = getUserID()
+emptyGame, gameID = checkAccessKey(userID)
+
+if emptyGame is True:
+    print("Add new game")
+else:
+    print("Please wait, statistics are loaded.")
+    generateStatistics(userID, gameID, cursor)
